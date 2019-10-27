e5ad978295af4006beaa7ee637393fbfc92aec47
Ming Ho <hkboi108@hotmail.com>
2019-10-26 17:59:19 -0700

    Add list and button components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
 import Select from './Components/Select';
+import AddButton from './Components/AddButton';
+import List from './Components/List';
 
 const App: React.FC = () => {
   // value of the selected field in the Select component
   const [selectedField, setSelectedField] = useState<string>('');
   // stores an array of values that has been added to the list component
   const [fieldList, setFieldList] = useState<string[] | undefined>([]);
+
   let items = ['apple', 'orange', 'banana', 'mango'];
+
+  const deleteListItem = (id: number) => {
+    setFieldList(fieldList && fieldList.filter((item, idx) => idx !== id ))
+  }
+
+  const checkButtonValue = () => {
+    if(selectedField === '') {
+      return true
+    } else {
+      return false
+    }
+  }
   return (
     <div className="App">
       <Select selectedField={selectedField} setSelectedField={setSelectedField} items={items}/>
+      <AddButton selectedField={selectedField} checkButtonValue={checkButtonValue} fieldList={fieldList} setFieldList={setFieldList}/>
+      <List fieldList={fieldList} deleteListItem={deleteListItem}/>
     </div>
   );
 }
